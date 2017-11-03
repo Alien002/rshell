955c8acacb1201f4cfe106fef73c20d0093cebb0
Daniel <dli015@ucr.edu>
2017-11-03 15:34:40 -0700

    messed up github + files and fixing

---
+++ single_command.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Tests for rshell:"
+
+echo "Test 1: echo A"
+echo A
+
+echo "Test 2: touch touch.cpp"
+touch touch.cpp
+
+echo "Test 3: ls"
+ls
+
+echo "Test 4: rm touch.cpp"
+rm touch.cpp
+
+echo "so far..."
