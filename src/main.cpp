d4f50dd63d4e5e675bcf1630b756b5dc5471cd09
Alic Lien <Alien002@ucr.edu>
2017-11-16 18:38:03 -0800

    Implemented test command

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 //  rshell - assignment 2
 //
 //  Created by Alic Lien & Daniel Li on 10/28/17.
-//  Updated 11/3/2017 - Hotfix
+//  Updated 11/16/2017 -
 //  Compiles via g++ main.cpp Command.cpp
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <vector>
-#include <string>
-#include <queue>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include "Command.hpp"
 #include "Execute.hpp"
 
@@ -33,6 +37,7 @@ void intro(){
          <<"\"; \" - will always run next command.\n"
          <<"type -q as a shell command to quit rShell.\n"
          <<"-------------------------------------------\n";
+    cout <<"!!!!!Dev Mode!!!!!\n";          
     return;
 }
 
@@ -47,7 +52,7 @@ void inputCommands(queue<string>& cmds){
                                                                 //git commit -m needs underscores for  ""
     for (int i = 0; i < v.size(); ++i){
         
-        if(v.at(i) == "#" || (v.at(i) != string() && v.at(i).at(0) == '#')){
+        if(v.at(i) == "#" || (v.at(i) != string() && v.at(i).at(0) == '#')){        //checks for comment #
             i = v.size();
         }
         else{
@@ -58,20 +63,114 @@ void inputCommands(queue<string>& cmds){
     return;
 }
 
+//handles the test command cases, uses stat.
+void isTest(queue<string>& temp, queue<string>& test, queue<string>& input){
+    input.pop();
+    
+    if(input.front() != "-e" && input.front() != "-f" && input.front() != "-d"){
+        
+        test.push("-e");
+        test.push(input.front());           //assuming input.front is a check
+        input.pop();
+    }
+    else{           //assumine is flag
+        test.push(input.front());       //push flag
+        input.pop();
+        test.push(input.front());       //push file path
+        input.pop();
+    }
+    
+    if(input.front() == "]"){
+        input.pop();
+    }
+    
+    //check flag, an
+    if(test.front() == "-e"){
+        test.pop();
+        //cout <<"-e flag" <<endl;
+        struct stat st;
+        if(stat(test.front().c_str(), &st) == 0){       //if true move on
+            cout <<"True" <<endl;
+            //temp.push("echo");
+        }
+        else{
+            cout <<"False" <<endl;
+            temp.push("la");
+        }
+        
+    }
+    else if(test.front() == "-f"){
+        test.pop();
+        //cout <<"-f flag" <<endl;
+        struct stat st;
+        stat(test.front().c_str(), &st);
+        
+        if(S_ISREG(st.st_mode)){                    //if true move on
+            cout <<"True" <<endl;
+            
+        }
+        else{
+            cout <<"False" <<endl;
+            temp.push("la");
+        }
+    }
+    else if(test.front() == "-d"){
+        test.pop();
+        //cout <<"-d flag" <<endl;
+        struct stat st;
+        stat(test.front().c_str(), &st);
+        
+        if(S_ISDIR(st.st_mode)){                    //if true move on
+            cout <<"True" <<endl;
+            
+        }
+        else{
+            cout <<"False" <<endl;                  //if false throw junk into temp queue to flag as failed for cmds
+            temp.push("la");
+        }
+    }
+    else{       //hopefully we never reach here
+        cout <<"This isn't suppose to happen, how did you get here, who are you, please leave." <<endl;
+        exit(1);
+    }
+    
+    /*
+     while(!test.empty()){                                       //used to check test queue strings.
+     cout <<"test input front: " <<test.front() <<endl;
+     test.pop();
+     }
+    */
+    
+    return;
+}
 
 
-void buildExecutable(queue<string>& input, vector<Command> &v){  //builds executable into a package
+//builds executable into a Command object, and
+void buildExecutable(queue<string>& input, vector<Command> &v){
     //cout <<"build executable\n";
     queue<string> temp;
+    queue<string> test; //only used for test
+    
     int sz = input.size();
     int i = 0;
     
     
     while(i != sz){
-        temp.push(input.front());
-        input.pop();
+        //check input.front if its a test case.
+        if(input.front() == "[" || input.front() == "test"){
 
+            isTest(temp, test, input);
+            
+            
+        }
+        else{   //normal commands
+            temp.push(input.front());
+            input.pop();
+        }
         
+        
+        
+        //connectors check
         if(input.front() == string()){
             input.pop();
             v.push_back(Command(temp, "; "));
@@ -99,6 +198,7 @@ void buildExecutable(queue<string>& input, vector<Command> &v){  //builds execut
     }
 }
 
+//Executes functions stored in Command object
 void execute(vector<Command> executables){
     vector<string> lhs;
     vector<string> rhs;
@@ -183,6 +283,7 @@ void execute(vector<Command> executables){
     }
     return;
 }
+
 
 
 int main(int argc, char** argv){
